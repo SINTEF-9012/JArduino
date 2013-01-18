c0c03f511a176d91c39e01dbff2fdaff232f7d85
brice-morin <brice.morin1@gmail.com>
2013-01-18 13:15:01 +0100

    GTalk example now using the Smack XMPP client library

--- jarduino.samples/src/main/java/org/sintef/jarduino/examples/advanced/GTalkAlert.java
+++ jarduino.samples/src/main/java/org/sintef/jarduino/examples/advanced/GTalkAlert.java
@@ -1,50 +1,53 @@
 /**
- * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June 2007;
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE, Version 3, 29 June
+ * 2007; you may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at
  *
- * 	http://www.gnu.org/licenses/lgpl-3.0.txt
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  *
- * Authors: Franck Fleurey and Brice Morin
- * Company: SINTEF IKT, Oslo, Norway
- * Date: 2011
+ * Authors: Brice Morin Company: SINTEF IKT, Oslo, Norway
+ * Date: 2013
  */
 package org.sintef.jarduino.examples.advanced;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.sintef.jarduino.DigitalPin;
 import org.sintef.jarduino.DigitalState;
 import org.sintef.jarduino.InterruptTrigger;
 import org.sintef.jarduino.JArduino;
 import org.sintef.jarduino.PinMode;
-
-/**
- * The libraries located in lib/voiceclearly are property of VoiceClealy:
- * Copyright 2009 VoiceClearly.
- * Please visit http://voiceclearly.com/voiceClearlyApiPricing.jsp
- * before reusing these libraries into your application.
- */
-import com.voiceclearly.api.VoiceClearlyService;
-import com.voiceclearly.api.chat.Chat;
-import com.voiceclearly.api.chat.Message;
-import com.voiceclearly.api.listener.CallListener;
-import com.voiceclearly.api.model.Call;
-import java.util.Date;
 import org.sintef.jarduino.InterruptPin;
 
 /**
- * Run the main according to the instructions and ask your friends to send
- * you instant messages on GTalk. Enjoy!
+ * This class is largely inspired by
+ * http://www.andrejkoelewijn.com/wp/2008/12/30/using-google-talk-from-java-example/
+ * and realized with the Smack API provided by Jive Software
+ * http://www.igniterealtime.org/index.jsp
  */
-public class GTalkAlert extends JArduino implements CallListener {
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
+
+/**
+ * Run the main according to the instructions and ask your friends to send you
+ * instant messages on GTalk. Click on the button to send him messages.
+ * Enjoy!
+ */
+public class GTalkAlert extends JArduino {
 
     private DigitalPin led;
     private InterruptPin button;
@@ -52,7 +55,7 @@ public class GTalkAlert extends JArduino implements CallListener {
     private String gmailUser;
     private String gmailPassword;
     private Timer timer;
-    private VoiceClearlyService service;
+    private XMPPConnection connection;
 
     public GTalkAlert(String port) {
         super(port);
@@ -60,34 +63,70 @@ public class GTalkAlert extends JArduino implements CallListener {
 
     public GTalkAlert(String port, String user, String pwd, String otherUser, DigitalPin led, InterruptPin button) {
         this(port);
+
         this.led = led;
         this.button = button;
         this.timer = new Timer();
         this.gmailUser = user;
         this.gmailPassword = pwd;
         this.gmailOtherUser = otherUser;
-        
-        attachInterrupt(this.button, InterruptTrigger.CHANGE);
-        
-        //Initialize the VoiceClearly Service with the listener
-        service = new VoiceClearlyService(this);
 
-        //Login with Google Talk credentials
-        service.login(gmailUser, gmailPassword);
+        attachInterrupt(this.button, InterruptTrigger.CHANGE);
+
+        try {
+            // connect to gtalk server
+            ConnectionConfiguration connConfig = new ConnectionConfiguration("talk.google.com", 5222, "gmail.com");
+            connection = new XMPPConnection(connConfig);
+            connection.connect();
+
+            // login with username and password
+            connection.login(gmailUser, gmailPassword);
+
+            // set presence status info
+            Presence presence = new Presence(Presence.Type.available);
+            connection.sendPacket(presence);
+
+
+
+            IncomingMessageHandler pl = new IncomingMessageHandler();
+            connection.addPacketListener(pl, null);
+
+        } catch (XMPPException ex) {
+            Logger.getLogger(GTalkAlert.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
+    /**
+     * When button is clicked, sends a message to your friend
+     *
+     * @param interrupt
+     */
     @Override
     protected void receiveInterruptNotification(InterruptPin interrupt) {
-    	System.out.println("Interrupt "+interrupt);
+        System.out.println("Interrupt " + interrupt);
         super.receiveInterruptNotification(interrupt);
         if (interrupt == button) {
-            service.sendIM(gmailOtherUser, "Button pushed: "+new Date(System.currentTimeMillis()));
+            // send a message to somebody
+            Message msg = new Message(gmailOtherUser, Message.Type.chat);
+            msg.setBody("hello");
+            connection.sendPacket(msg);
         }
     }
 
     public void turnOffLED() {
         digitalWrite(led, DigitalState.LOW);
         System.out.println("Hopefully, the LED should now be turned OFF");
+    }
+
+    public void turnOnLED() {
+        digitalWrite(led, DigitalState.HIGH);
+        System.out.println("Hopefully, the LED should now be turned ON");
+    }
+
+    public void turnOnLEDfor(long delay) {
+        turnOnLED();
+        timer.schedule(new Timeout(), delay);
     }
 
     @Override
@@ -101,51 +140,40 @@ public class GTalkAlert extends JArduino implements CallListener {
 
     private class Timeout extends TimerTask {
 
-        GTalkAlert gTalk;
-
-        public Timeout(GTalkAlert gTalk) {
-            this.gTalk = gTalk;
-        }
-
         @Override
         public void run() {
-            gTalk.turnOffLED();
+            turnOffLED();
         }
     }
 
-    public void incomingCall(Call call) {
-    }
-
-    public void callAnswered(Call call) {
-    }
-
-    public void callDeclined(Call call, String reason) {
-    }
-
-    public void callDisconnected(Call call, String reason) {
-    }
-
-    public void incomingIm(Chat chat, Message message) {
-        digitalWrite(led, DigitalState.HIGH);
-        System.out.println("Hopefully, the LED should now be turned ON");
-        timer.schedule(new Timeout(this), 10000);
+    private class IncomingMessageHandler implements PacketListener {
+        
+        @Override
+        public void processPacket(Packet p) {
+            System.out.println("Incoming message from " + p.getFrom() + ": " + p.toString());
+            if (p instanceof Message) {
+                Message msg = (Message) p;
+                System.out.println(msg.getFrom() + ": " + msg.getBody());
+                turnOnLEDfor(5000);
+            }
+        }
+        
     }
 
     /**
-     * Please set the serial port with the proper port,
-     * the credentials of your Google Account, as well as the PIN 
-     * where your LED is connected on your Arduino board.
-     * Disclaimer: We are not responsible for any troubles with your google account. 
-     * The connection to your Google account is entirely delegated to the
-     * VoiceClearly APIs: http://voiceclearly.com/googleTalkVoiceClearly.jsp
+     * Please set the serial port with the proper port, the credentials of your
+     * Google Account, as well as the PIN where your LED is connected on your
+     * Arduino board. Disclaimer: We are not responsible for any troubles with
+     * your google account. The connection to your Google account is entirely
+     * delegated to the Smack API
      */
     public static void main(String[] args) {
-        String otherName = "yourfriend@gmail.com";
-        String userName = "first.last";
-        String password = "myNicePassword";
-        String serialPort = "COM8";
+        String otherName = "your.friend@gmail.com";
+        String userName = "first.last@gmail.com";
+        String password = "yourPassword";
+        String serialPort = "COM4";
 
-        DigitalPin led = DigitalPin.PIN_7;
+        DigitalPin led = DigitalPin.PIN_9;
         InterruptPin button = InterruptPin.PIN_2_INT0;
 
         JArduino arduino = new GTalkAlert(serialPort, userName, password, otherName, led, button);
